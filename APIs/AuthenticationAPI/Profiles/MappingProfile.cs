bb237be15e8ed716d6851d06dc07b3c7de555b21
ersinisgor <ersinisgor@hotmail.com>
2025-05-20 22:38:23 +0300

    feat(AuthenticationAPI): Add AutoMapper profile for auth mappings

---
+++ APIs/AuthenticationAPI/Profiles/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using AuthenticationAPI.Models;
+using AuthenticationAPI.DTOs;
+using AuthenticationAPI.Commands;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AuthenticationAPI.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>();
+            CreateMap<RegisterCommand, User>();
+        }
+    }
+}
