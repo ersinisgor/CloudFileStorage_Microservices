9ca58cdd4bc24a3e36d6828c232343cdec06e219
ersinisgor <ersinisgor@hotmail.com>
2025-05-20 11:17:06 +0300

    feat(FileMetaDataAPI): Add FileShare model for access control

---
+++ APIs/FileMetaDataAPI/Models/FileShare.cs
@@ -0,0 +1,9 @@
+﻿namespace FileMetaDataAPI.Models
+{
+    public class FileShare
+    {
+        public int FileId { get; set; }
+        public int UserId { get; set; }
+        public string Permission { get; set; }
+    }
+}
