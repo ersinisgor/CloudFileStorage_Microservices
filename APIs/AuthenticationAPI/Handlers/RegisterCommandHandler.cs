70c13ea2ba3ffc6a3ce0729867f1d513349c8d0e
ersinisgor <ersinisgor@hotmail.com>
2025-05-20 22:49:23 +0300

    refactor(AuthenticationAPI): Implement AutoMapper in RegisterCommandHandler

---
+++ APIs/AuthenticationAPI/Handlers/RegisterCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using AuthenticationAPI.DataContext;
+using AuthenticationAPI.Models;
+using AuthenticationAPI.DTOs;
+using AuthenticationAPI.Commands;
+using AutoMapper;
+
+namespace AuthenticationAPI.Handlers
+{
+    public class RegisterCommandHandler(ApplicationDbContext context, IMapper mapper) : IRequestHandler<RegisterCommand, UserDTO>
+    {
+        public async Task<UserDTO> Handle(RegisterCommand request, CancellationToken cancellationToken)
+        {
+            if (context.Users.Any(u => u.Email == request.Email))
+                throw new Exception("User already exist");
+
+            var user = mapper.Map<User>(request);
+            user.PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            user.Role = "User";
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return mapper.Map<UserDTO>(user);
+        }
+    }
+}
