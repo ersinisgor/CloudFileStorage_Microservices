995317e42c1bf018ecfa9b26b3d416829abd0c5d
ersinisgor <ersinisgor@hotmail.com>
2025-05-20 20:37:53 +0300

    feat(AuthenticationAPI): Implement user registration with CQRS pattern

---
+++ APIs/AuthenticationAPI/Commands/RegisterCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using AuthenticationAPI.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationAPI.Commands
+{
+    public class RegisterCommand : IRequest<UserDTO>
+    {
+        [Required]
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
