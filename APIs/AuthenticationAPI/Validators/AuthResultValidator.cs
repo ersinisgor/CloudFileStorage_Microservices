b5a4ac0a045e4a05053b9b99a715f23e1cd938c6
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 04:34:20 +0300

    feat(AuthenticationAPI): Add AuthResultValidator for token response validation

---
+++ APIs/AuthenticationAPI/Validators/AuthResultValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using AuthenticationAPI.DTOs;
+
+namespace AuthenticationAPI.Validators
+{
+    public class AuthResultValidator : AbstractValidator<AuthResult>
+    {
+        public AuthResultValidator()
+        {
+            RuleFor(x => x.Token)
+                .NotEmpty().WithMessage("The JWT {PropertyName} field is required.");
+            RuleFor(x => x.RefreshToken)
+                .NotEmpty().WithMessage("The {PropertyName} field is required.");
+        }
+    }
+}
