f4549d01a8b4c4a7790c44190f22f4c7319258f3
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 05:27:41 +0300

    refactor(AuthenticationAPI): Remove DataAnnotations in favor of FluentValidation

--- APIs/AuthenticationAPI/Queries/LoginQuery.cs
+++ APIs/AuthenticationAPI/Queries/LoginQuery.cs
@@ -1,14 +1,11 @@
 ﻿using MediatR;
 using AuthenticationAPI.DTOs;
-using System.ComponentModel.DataAnnotations;
 
 namespace AuthenticationAPI.Queries
 {
     public class LoginQuery : IRequest<AuthResult>
     {
-        [EmailAddress]
         public string Email { get; set; }
-        [Required]
         public string Password { get; set; }
     }
 }
