b1e0dfe46ef50784d8fac5797f82d2bd5cf8370d
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 13:56:32 +0300

    feat(FileMetadataAPI): Add File and FileShare DTOs

---
+++ APIs/FileMetadataAPI/DTOs/FileDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FileMetadataAPI.DTOs
+{
+    public class FileDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int OwnerId { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string Visibility { get; set; }
+        public List<FileShareDTO> FileShares { get; set; } = new();
+    }
+}
