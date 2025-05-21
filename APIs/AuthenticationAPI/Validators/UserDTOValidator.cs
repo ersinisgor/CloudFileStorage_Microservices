1d228f63ebea4f6a629f3c30c91e10c38459c5b6
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 04:31:40 +0300

    feat(AuthenticationAPI): Add UserDTO validation for API responses

---
+++ APIs/AuthenticationAPI/Validators/UserDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using AuthenticationAPI.DTOs;
+
+namespace AuthenticationAPI.Validators
+{
+    public class UserDTOValidator : AbstractValidator<UserDTO>
+    {
+        public UserDTOValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0).WithMessage("The user ID must be a positive value.");
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("The {PropertyName} field is required.")
+                .MaximumLength(50).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters.");
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("The {PropertyName} field is required.")
+                .EmailAddress().WithMessage("Enter a valid email address.");
+        }
+    }
+}
