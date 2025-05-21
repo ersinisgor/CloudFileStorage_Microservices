0e007aabd2f3e5f7bb650321a9ac55f54988f5c7
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 11:50:56 +0300

    feat(FileMetadataAPI): Configure entity relationships in ApplicationDbContext

--- APIs/FileMetadataAPI/DataContext/ApplicationDbContext.cs
+++ APIs/FileMetadataAPI/DataContext/ApplicationDbContext.cs
@@ -14,6 +14,17 @@ namespace FileMetadataAPI.DataContext
         public DbSet<File> Files { get; set; }
         public DbSet<FileShare> FileShares { get; set; }
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<File>()
+                .HasMany(f => f.FileShares)
+                .WithOne(fs => fs.File)
+                .HasForeignKey(fs => fs.FileId);
+
+            modelBuilder.Entity<FileShare>()
+                .HasKey(fs => new { fs.FileId, fs.UserId });
+        }
+
+
     }
 }
