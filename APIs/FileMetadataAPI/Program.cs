2a8b9562e26acbfff1ea980bfb393ee58fc169c4
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 11:38:18 +0300

    feat(FileMetadataAPI): Add PostgreSQL database integration to Program.cs

--- APIs/FileMetadataAPI/Program.cs
+++ APIs/FileMetadataAPI/Program.cs
@@ -1,9 +1,18 @@
+using FileMetadataAPI.DataContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    if (string.IsNullOrEmpty(connectionString))
+        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    options.UseNpgsql(connectionString);
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
