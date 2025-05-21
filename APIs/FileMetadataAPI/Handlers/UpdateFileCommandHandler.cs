2d4881c0fa455b06727e981b035238e7fa4dc4e3
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 21:24:58 +0300

    feat(FileMetadataAPI): Implement file update functionality with validation

---
+++ APIs/FileMetadataAPI/Handlers/UpdateFileCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using FileMetadataAPI.DataContext;
+using FileMetadataAPI.DTOs;
+using AutoMapper;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using FileMetadataAPI.Commands;
+using FileShare = FileMetadataAPI.Models.FileShare;
+
+namespace FileMetadataAPI.Handlers
+{
+    internal class UpdateFileCommandHandler(
+        ApplicationDbContext context,
+        IMapper mapper,
+        IHttpContextAccessor httpContextAccessor) : IRequestHandler<UpdateFileCommand, FileDTO>
+    {
+        public async Task<FileDTO> Handle(UpdateFileCommand request, CancellationToken cancellationToken)
+        {
+            var userId = int.Parse(httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value
+                ?? throw new Exception("User ID not found."));
+
+            var file = await context.Files
+                .Include(f => f.FileShares)
+                .FirstOrDefaultAsync(f => f.Id == request.Id && f.OwnerId == userId, cancellationToken)
+                ?? throw new Exception("File not found or authorization error.");
+
+            mapper.Map(request, file);
+            file.FileShares.Clear();
+            file.FileShares.AddRange(mapper.Map<List<FileShare>>(request.FileShares));
+
+            await context.SaveChangesAsync(cancellationToken);
+
+            return mapper.Map<FileDTO>(file);
+        }
+    }
+}
