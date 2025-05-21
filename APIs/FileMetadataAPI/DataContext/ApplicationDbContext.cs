6993538db575d6fe9165236881567354e969563b
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 11:37:03 +0300

    feat(FileMetadataAPI): Add ApplicationDbContext with File and FileShare entities

---
+++ APIs/FileMetadataAPI/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using File = FileMetadataAPI.Models.File;
+using FileShare = FileMetadataAPI.Models.FileShare;
+
+namespace FileMetadataAPI.DataContext
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<File> Files { get; set; }
+        public DbSet<FileShare> FileShares { get; set; }
+
+        
+    }
+}
