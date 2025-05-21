974f1f6f31a9e54b2f42a3fcf17a13f802db3734
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 06:11:25 +0300

    refactor(AuthenticationAPI): Revert User model encapsulation changes

--- APIs/AuthenticationAPI/Models/User.cs
+++ APIs/AuthenticationAPI/Models/User.cs
@@ -1,10 +1,13 @@
-﻿internal class User
+﻿namespace AuthenticationAPI.Models
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PasswordHash { get; private set; }
-    public string Role { get; set; }
-    public string? RefreshToken { get; private set; }
-    public DateTime? RefreshTokenExpiry { get; private set; }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiry { get; set; }
+    }
 }
