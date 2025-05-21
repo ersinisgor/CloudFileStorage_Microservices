e92826cce202c2c68d25f8abf637a63f0a9cafe8
ersinisgor <ersinisgor@hotmail.com>
2025-05-21 05:47:05 +0300

    refactor(AuthenticationAPI): Encapsulate sensitive User model properties

--- APIs/AuthenticationAPI/Models/User.cs
+++ APIs/AuthenticationAPI/Models/User.cs
@@ -1,13 +1,10 @@
-﻿namespace AuthenticationAPI.Models
+﻿internal class User
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
-        public string Role { get; set; }
-        public string? RefreshToken { get; set; }
-        public DateTime? RefreshTokenExpiry { get; set; }
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PasswordHash { get; private set; }
+    public string Role { get; set; }
+    public string? RefreshToken { get; private set; }
+    public DateTime? RefreshTokenExpiry { get; private set; }
 }
