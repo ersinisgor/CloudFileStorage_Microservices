00629301d792045ec846cfc7608c60c82357c98c
ersinisgor <ersinisgor@hotmail.com>
2025-05-22 06:24:14 +0300

    feat(FileStorageAPI): Add file upload DTO and mapping profile

---
+++ APIs/FileStorageAPI/Profiles/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using FileStorageAPI.Commands.UploadFile;
+using FileStorageAPI.DTOs;
+
+namespace FileStorageAPI.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UploadFileRequest, UploadFileCommand>();
+        }
+    }
+}
