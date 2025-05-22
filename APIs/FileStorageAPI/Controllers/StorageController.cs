334e001965a81b2633f6bdd44885e9cfc8a12ac3
ersinisgor <ersinisgor@hotmail.com>
2025-05-22 06:57:53 +0300

    feat(StorageController): Complete file transfer API with download

--- APIs/FileStorageAPI/Controllers/StorageController.cs
+++ APIs/FileStorageAPI/Controllers/StorageController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using MediatR;
 using FileStorageAPI.Commands.UploadFile;
+using FileStorageAPI.DTOs;
+using FileStorageAPI.Queries.DownloadFile;
 
 
 namespace FileStorageAPI.Controllers
@@ -19,5 +21,12 @@ namespace FileStorageAPI.Controllers
             return Ok(result);
         }
 
+        [HttpGet("download")]
+        public async Task<IActionResult> Download([FromQuery] DownloadFileRequest request)
+        {
+            var query = new DownloadFileQuery { FilePath = request.FilePath };
+            var result = await mediator.Send(query);
+            return File(result.Content, result.ContentType, result.FileName);
+        }
     }
 }
