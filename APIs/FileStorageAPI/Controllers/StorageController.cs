f362d55d8262f3e4cbd1854af71c9901c0befe6f
ersinisgor <ersinisgor@hotmail.com>
2025-05-22 06:34:21 +0300

    feat(FileStorageAPI): Add authorized file upload controller

--- APIs/FileStorageAPI/Controllers/StorageController.cs
+++ APIs/FileStorageAPI/Controllers/StorageController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using FileStorageAPI.Commands.UploadFile;
+
 
 namespace FileStorageAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StorageController : ControllerBase
+    [Authorize]
+    public class StorageController(IMediator mediator) : ControllerBase
     {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload([FromForm] IFormFile file)
+        {
+            var command = new UploadFileCommand { File = file };
+            var result = await mediator.Send(command);
+            return Ok(result);
+        }
+
     }
 }
