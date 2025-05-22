98bcd78afc9c952e6394363088bdb78ac3b6cb12
ersinisgor <ersinisgor@hotmail.com>
2025-05-22 06:23:45 +0300

    feat(FileStorageAPI): Add FileStorage model

---
+++ APIs/FileStorageAPI/Models/FileStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace FileStorageAPI.Models
+{
+    public class FileStorage
+    {
+        public byte[] Content { get; set; }
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+    }
+}
