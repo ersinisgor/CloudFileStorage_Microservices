3185f73b6df2a7da5bb3841c51a7c94288058158
ersinisgor <ersinisgor@hotmail.com>
2025-05-22 06:25:31 +0300

    feat(FileStorageAPI): Implement file upload validation

---
+++ APIs/FileStorageAPI/Validators/UploadFileRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FileStorageAPI.DTOs;
+
+namespace FileStorageAPI.Validators
+{
+    public class UploadFileRequestValidator : AbstractValidator<UploadFileRequest>
+    {
+        public UploadFileRequestValidator()
+        {
+            Int32 maxFileSize = 10 * 1024 * 1024;
+            RuleFor(x => x.File)
+                .NotNull().WithMessage("File is required.")
+                .Must(file => file.Length > 0).WithMessage("File cannot be empty.")
+                .Must(file => file.Length <= maxFileSize).WithMessage("File size cannot exceed 10MB.");
+        }
+    }
+}
