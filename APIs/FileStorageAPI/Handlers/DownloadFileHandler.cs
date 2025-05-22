f5cfefd96f7e97c668cd3642edef3f869b50ae8a
ersinisgor <ersinisgor@hotmail.com>
2025-05-22 06:44:35 +0300

    feat(FileStorageAPI): Implement file download query and handler

---
+++ APIs/FileStorageAPI/Handlers/DownloadFileHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using FileStorageAPI.Queries.DownloadFile;
+using FileStorageAPI.Models;
+using System.Net.Mime;
+
+namespace FileStorageAPI.Handlers
+{
+    public class DownloadFileHandler : IRequestHandler<DownloadFileQuery, FileStorage>
+    {
+        public async Task<FileStorage> Handle(DownloadFileQuery request, CancellationToken cancellationToken)
+        {
+            if (!File.Exists(request.FilePath))
+            {
+                throw new FileNotFoundException("File not found.");
+            }
+
+            var content = await File.ReadAllBytesAsync(request.FilePath, cancellationToken);
+            var contentType = MediaTypeNames.Application.Octet;
+            var fileName = Path.GetFileName(request.FilePath);
+
+            return new FileStorage
+            {
+                Content = content,
+                ContentType = contentType,
+                FileName = fileName
+            };
+        }
+    }
+}
