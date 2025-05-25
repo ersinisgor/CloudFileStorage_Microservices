dec2efc553b0ddb497a049a4e7b3bfacf27b0c94
ersinisgor <ersinisgor@hotmail.com>
2025-05-25 21:45:14 +0300

    feat(FileMetadataAPI): Enhance data models for file storage and sharing

--- APIs/FileMetadataAPI/Models/FileShare.cs
+++ APIs/FileMetadataAPI/Models/FileShare.cs
@@ -2,9 +2,10 @@
 {
     internal class FileShare
     {
+        public int Id { get; set; }
         public int FileId { get; set; }
-        public File File { get; set; }
         public int UserId { get; set; }
         public string Permission { get; set; }
+        public File File { get; set; }
     }
 }
