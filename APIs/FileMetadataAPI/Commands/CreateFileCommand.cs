b021b799f63cbc01dc2531f11670dbf7f0de66b6
ersinisgor <ersinisgor@hotmail.com>
2025-05-25 21:41:12 +0300

    feat(FileMetadataAPI): Implement file storage integration in CreateFileCommandHandler

--- APIs/FileMetadataAPI/Commands/CreateFileCommand.cs
+++ APIs/FileMetadataAPI/Commands/CreateFileCommand.cs
@@ -8,6 +8,7 @@ namespace FileMetadataAPI.Commands
         public string Name { get; set; }
         public string Description { get; set; }
         public string Visibility { get; set; }
+        public IFormFile File { get; set; } // Added for file upload
         public List<FileShareDTO> FileShares { get; set; } = new();
     }
 }
