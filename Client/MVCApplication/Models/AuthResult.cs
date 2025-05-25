ff37cfffad78e9c8c623b521904bc11c3f693b95
ersinisgor <ersinisgor@hotmail.com>
2025-05-25 16:50:56 +0300

    feat(MVCApplication): Add AuthResult model

---
+++ Client/MVCApplication/Models/AuthResult.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCApplication.Models
+{
+    public class AuthResult
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+        public UserInfo User { get; set; }
+    }
+
+    public class UserInfo
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
