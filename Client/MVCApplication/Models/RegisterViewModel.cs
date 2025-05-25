d1f5bfd4152556115ffac171141cb6c94c6a271b
ersinisgor <ersinisgor@hotmail.com>
2025-05-25 16:53:04 +0300

    feat(MVCApplication): Add RegisterViewModel

---
+++ Client/MVCApplication/Models/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCApplication.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "The Name field is required.")]
+        [StringLength(50, ErrorMessage = "The Name cannot be longer than 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The Email field is required.")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The Password field is required.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "The Password must be at least 8 characters.")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).+$", ErrorMessage = "The password must contain at least one capital letter, one number, and one special character (!@#$%^&*).")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "The Role field is required.")]
+        public string Role { get; set; }
+    }
+}
