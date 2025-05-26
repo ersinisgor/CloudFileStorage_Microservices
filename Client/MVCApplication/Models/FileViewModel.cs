41f6f801665513899b48fff51ae7d8fdb148992c
ersinisgor <ersinisgor@hotmail.com>
2025-05-26 11:50:36 +0300

    feat(MVCApplication): Add IsOwner property to FileViewModel

--- Client/MVCApplication/Models/FileViewModel.cs
+++ Client/MVCApplication/Models/FileViewModel.cs
@@ -4,12 +4,13 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int OwnerId { get; set; }
         public DateTime UploadDate { get; set; }
         public string Visibility { get; set; }
         public string Path { get; set; }
         public List<FileShareViewModel> FileShares { get; set; } = new();
+        public bool IsOwner { get; set; }
     }
 
     public class FileShareViewModel
