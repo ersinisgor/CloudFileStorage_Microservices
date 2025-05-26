450eaa2b68d35b949f56e5d3343e5fefd4c25672
ersinisgor <ersinisgor@hotmail.com>
2025-05-26 04:42:25 +0300

    refactor(FileMetadataAPI): Improve null safety for description field in DTOs and commands

--- APIs/FileMetadataAPI/Commands/CreateFileCommand.cs
+++ APIs/FileMetadataAPI/Commands/CreateFileCommand.cs
@@ -7,7 +7,7 @@ namespace FileMetadataAPI.Commands
     public class CreateFileCommand : IRequest<FileDTO>
     {
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public string Visibility { get; set; }
         public IFormFile File { get; set; } // Added for file upload
         public List<FileShareDTO> FileShares { get; set; } = new();
--- APIs/FileMetadataAPI/DTOs/FileDTO.cs
+++ APIs/FileMetadataAPI/DTOs/FileDTO.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int OwnerId { get; set; }
         public DateTime UploadDate { get; set; }
         public string Visibility { get; set; }
