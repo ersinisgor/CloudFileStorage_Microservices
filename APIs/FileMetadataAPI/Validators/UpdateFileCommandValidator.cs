9db6a2697f571b2ad2190d7b87292f554d288ad6
ersinisgor <ersinisgor@hotmail.com>
2025-05-31 11:22:52 +0300

    refactor(FileMetadataAPI): Enhanced and standardized validation for `FileShares` and enum properties

--- APIs/FileMetadataAPI/Validators/UpdateFileCommandValidator.cs
+++ APIs/FileMetadataAPI/Validators/UpdateFileCommandValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using FileMetadataAPI.Commands;
+using FileMetadataAPI.Models;
 
 namespace FileMetadataAPI.Validators
 {
@@ -16,8 +17,16 @@ namespace FileMetadataAPI.Validators
                 .MaximumLength(1000).WithMessage("The {PropertyName} cannot be longer than {MaxLength} characters.");
             RuleFor(x => x.Visibility)
                 .NotEmpty().WithMessage("The {PropertyName} field is required.")
-                .Must(v => Enum.TryParse<Models.Visibility>(v, out _))
+                .Must(v => Enum.TryParse<Visibility>(v, out _))
                 .WithMessage("Invalid visibility value (Private, Public, Shared).");
+            When(x => x.Visibility == "Private" || x.Visibility == "Public", () =>
+            {
+                RuleFor(x => x.FileShares).Empty().WithMessage("FileShares must be empty when visibility is Private or Public.");
+            });
+            When(x => x.Visibility == "Shared", () =>
+            {
+                RuleFor(x => x.FileShares).NotEmpty().WithMessage("FileShares must not be empty when visibility is Shared.");
+            });
             RuleForEach(x => x.FileShares)
                 .SetValidator(new FileShareDTOValidator());
         }
