4839bfa17a7cf4967eac180f20ce82a9151b7b0a
ersinisgor <ersinisgor@hotmail.com>
2025-06-02 08:21:33 +0300

    feat(MVCApplication): Add data annotations for UpdateFileViewModel, Remove unused UpdateFileDTO model

--- Client/MVCApplication/Models/UpdateFileViewModel.cs
+++ Client/MVCApplication/Models/UpdateFileViewModel.cs
@@ -1,12 +1,23 @@
-﻿namespace MVCApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCApplication.Models
 {
     public class UpdateFileViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "File name is required.")]
+        [MaxLength(255, ErrorMessage = "The file name cannot be longer than 255 characters.")]
         public string Name { get; set; }
+
+        [MaxLength(1000, ErrorMessage = "The description cannot be longer than 1000 characters.")]
         public string? Description { get; set; }
+
+        [Required(ErrorMessage = "The visibility field is required.")]
         public string Visibility { get; set; }
-        public string? SharedUserIds { get; set; } 
+
+        public string? SharedUserIds { get; set; }
+
         public string Permission { get; set; }
     }
 }
