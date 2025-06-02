2d4c70e7aeafda795d0fbeb00859577db1fbf222
ersinisgor <ersinisgor@hotmail.com>
2025-06-02 07:49:33 +0300

    refactor(MVCApplication): Make DTO and ViewModel properties nullable

--- Client/MVCApplication/Models/UpdateFileDTO.cs
+++ Client/MVCApplication/Models/UpdateFileDTO.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public string Visibility { get; set; }
-        public string SharedUserIds { get; set; }
+        public string? SharedUserIds { get; set; }
         public string Permission { get; set; }
     }
 }
